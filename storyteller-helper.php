d44aae7c2b852dddaf0bd78321f5663f907cb225
Josiah Sprague <josiah.sprague@gmail.com>
2015-05-20 16:02:57 -0400

    stub action hooks and functions

--- storyteller-helper.php
+++ storyteller-helper.php
@@ -11,3 +11,20 @@
 
 // Blocks direct access to plugin PHP files
 defined( 'ABSPATH' ) or die( 'Access denied!' );
+
+// WordPress Action Reference: https://codex.wordpress.org/Plugin_API/Action_Reference
+function clear_storyteller_post_cache() {
+  // get_post($post_id) should return page or post id here
+  // Clear /posts
+  // Clear /pages
+}
+add_action('save_post', 'clear_storyteller_post_cache');
+add_action('publish_future_post', 'clear_storyteller_post_cache');
+add_action('deleted_post', 'clear_storyteller_post_cache');
+
+function clear_storyteller_attachment_cache() {
+  // Clear /media
+}
+add_action('add_attachment', 'clear_storyteller_attachment_cache');
+add_action('edit_attachment', 'clear_storyteller_attachment_cache');
+add_action('delete_attachment', 'clear_storyteller_attachment_cache');
