02316dea2edf901d75f49b28b430b1c32ea4ff8d
Josiah Sprague <josiah.sprague@gmail.com>
2015-05-20 11:42:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Storyteller Helper
+A WordPress plugin that adds useful features for use with Storyteller.io such as triggering webhooks to clear the Storyteller cache on post updates.
---
+++ storyteller-helper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Storyteller Helper
+ * Plugin URI:  https://github.com/SparkartGroupInc/storyteller-helper
+ * Description: Adds useful features for use with Storyteller.io such as triggering webhooks to clear the Storyteller cache on post updates.
+ * Version:     0.0.0
+ * Author:      Sparkart Group, Inc.
+ * Author URI:  http://sparkart.com
+ * License:     MIT
+ */
+
+// Blocks direct access to plugin PHP files
+defined( 'ABSPATH' ) or die( 'Access denied!' );
