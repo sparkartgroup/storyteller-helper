d862d10d86f20d922fddaefef2426465385364b9
Josiah Sprague <josiah.sprague@gmail.com>
2015-08-10 12:30:43 -0400

    v1.0.0

--- storyteller-helper.php
+++ storyteller-helper.php
@@ -3,7 +3,7 @@
  * Plugin Name: Storyteller Helper
  * Plugin URI:  https://github.com/SparkartGroupInc/storyteller-helper
  * Description: Adds useful features for use with Storyteller.io such as triggering webhooks to clear the Storyteller cache on post updates.
- * Version:     0.1.0
+ * Version:     1.0.0
  * Author:      Sparkart Group, Inc.
  * Author URI:  http://sparkart.com
  * License:     MIT
